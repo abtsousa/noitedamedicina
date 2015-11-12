5b0ae874bf5023844666bcb5cce44cd7e709fa4e
noitedamedicina <noitedamedicina@aefml.pt>
2015-11-12 22:19:22 +0000

    For real tho

--- js/randombg.js
+++ js/randombg.js
@@ -1,4 +1,4 @@
 $(function() {
     var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg'];
-    $('html, body').css({'background-image': 'url(/bg/ndm15/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    $('.site-wrapper').css({'background-image': 'url(/bg/ndm15/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
